9ba776f4e3985b0f2ac6635f91e9527155436e79
John Thomas Gause III <johntgause12@gmail.com>
2024-02-08 01:52:21 -0800

    js

--- js/script.js
+++ js/script.js
@@ -1,21 +1,81 @@
 'use strict';
 
+let dataFromStorage = fetchData();
+console.log('here is our data', dataFromStorage);
 let products = [];
 let image1 = document.getElementById('image1');
 let image2 = document.getElementById('image2');
 let image3 = document.getElementById('image3');
-let totalRounds = 25;
-let currentRound= 0;
 
-// button that clears the DATA IN STORAGE
-const ctx = document.getElementById('myChart');
-let clearButton = document.getElementById('start-over');
-clearButton.addEventListener('click', function () {
-  clearData();
-});
-// ^^^^^^ CLEARS DATA IN STORAGE ^^^^^
+// condition that determines what state our app is in
+if (dataFromStorage) {
+  products = dataFromStorage;
+  let product1 = products[0];
+  console.log('HERE IS GOAT FROM STORAGE', product1);
+  let product2 = products[1];
+  let product3 = products[2];
 
-// constructor function -> 'this' is the object we are creating.
+  image1.setAttribute('src', product1.url);
+  image2.setAttribute('src', product2.url);
+  image3.setAttribute('src', product3.url);
+  product1.timesShown++;
+  image1.setAttribute('alt', product1.name);
+  image2.setAttribute('alt', product2.name);
+  image3.setAttribute('alt', product3.name);
+  product2.timesShown++;
+} else {
+  let product1 = new Product('img/img/bag.jpg', 'BAG');
+  let product2 = new Product('img/img/banana.jpg', 'BANANA');
+  let product3 = new Product('img/img/bathroom.jpg', 'BATHROOM');
+  let product4 = new Product('img/img/boots.jpg', 'BOOTS');
+  let product5 = new Product('img/img/breakfast.jpg', 'BREAKFAST');
+  let product6 = new Product('img/img/bubblegum.jpg', 'BUBBLEGUM');
+  let product7 = new Product(img/img/chair.jpg, CHAIR);
+  let product8 = new Product(img/img/cthulhu.jpg, CTHULHU);
+  let product9 = new Product(img/img/dog-duck.jpg, DOG-DUCK);
+  let product10 = new Product(img/img/dragon.jpg, DRAGON);
+  let product11 = new Product(img/img/pen.jpg, PEN);
+  let product12 = new Product(img/img/pet-sweep.jpg, PET-SWEEP);
+  let product13 = new Product(img/img/scissors.jpg, SCISSORS);
+  let product14 = new Product(img/img/shark.jpg, SHARK);
+  let product15 = new Product(img/img/sweep.png, SWEEP);
+  let product16 = new Product(img/img/tauntaun.jpg, TAUNTAUN);
+  let product17 = new Product(img/img/unicorn.jpg, UNICORN);
+  let product18 = new Product(img/img/water-can.jpg, WATER-CAN);
+  let product19 = new Product(img/img/wine-glass.jpg, WINE-GLASS);
+
+  products.push(
+    product1,
+    product2,
+    product3,
+    product4,
+    product5,
+    product6,
+    product7,
+    product8,
+    product9,
+    product10,
+    product11,
+    product12,
+    product13,
+    product14,
+    product15,
+    product16,
+    product17,
+    product18,
+    product19);
+
+  // render the goat onto the page / add the name
+  image1.setAttribute("src", product1.url);
+  image1.setAttribute("alt", product1.name);
+  product1.timesShown++;
+  image2.setAttribute("src", product2.url);
+  image2.setAttribute("alt", product2.name);
+  product2.timesShown++;
+  image3.setAttribute("src", product3.url);
+  image3.setAttribute("alt", product3.name);
+}
+
 function Product(url, name) {
   this.url = url;
   this.name = name;
@@ -23,59 +83,6 @@ function Product(url, name) {
   this.timesShown = 0;
 }
 
-let product1 = new Product('img/img/bag.jpg', 'BAG');
-let product2 = new Product('img/img/banana.jpg', 'BANANA');
-let product3 = new Product('img/img/bathroom.jpg', 'BATHROOM');
-let product4 = new Product('img/img/boots.jpg', 'BOOTS');
-let product5 = new Product('img/img/breakfast.jpg', 'BREAKFAST');
-let product6 = new Product('img/img/bubblegum.jpg', 'BUBBLEGUM');
-let product7 = new Product('img/img/chair.jpg', 'CHAIR');
-let product8 = new Product('img/img/cthulhu.jpg', 'CTHULHU');
-let product9 = new Product('img/img/dog-duck.jpg', 'DOG-DUCK');
-let product10 = new Product('img/img/dragon.jpg', 'DRAGON');
-let product11 = new Product('img/img/pen.jpg', 'PEN');
-let product12 = new Product('img/img/pet-sweep.jpg', 'PET-SWEEP');
-let product13 = new Product('img/img/scissors.jpg', 'SCISSORS');
-let product14 = new Product('img/img/shark.jpg', 'SHARK');
-let product15 = new Product('img/img/sweep.png', 'SWEEP');
-let product16 = new Product('img/img/tauntaun.jpg', 'TAUNTAUN');
-let product17 = new Product('img/img/unicorn.jpg', 'UNICORN');
-let product18 = new Product('img/img/water-can.jpg', 'WATER-CAN');
-let product19 = new Product('img/img/wine-glass.jpg', 'WINE-GLASS');
-
-
-products.push(product1, product2, product3, product4, product5, product6, product7, product8, product9, product10, product11, product12, product13, product14, product15, product16, product17, product18, product19);
-
-function renderImage(image, product) {
-  image.setAttribute('src' , product.url);
-  image.setAttribute('alt' , product.name);
-  product.timesShown++;
-}
-
-renderImage(image1, product1);
-renderImage(image2, product2);
-renderImage(image3, product3);
-
-// chatGPT
-
-
-console.log(products);
-
-// render the prodcuts onto the page / add the name
-// image1.setAttribute('src', product1.url);
-// image1.setAttribute('alt', product1.name);
-// product1.timesShown++;
-// image2.setAttribute('src', product2.url);
-// image2.setAttribute('alt', product2.name);
-// product2.timesShown++;
-// image3.setAttribute('src', product3.url);
-// image3.setAttribute('alt', product3.name);
-// product3.timesShown++;
-
-// console.log(products);
-
-
-// add an event listener that runs some code when a goat picture is clicked.
 let productImages = document.getElementById('products');
 
 // when might you remove the event listener from the GoatImages HTML element
@@ -97,8 +104,8 @@ let handleClick = function(event) {
 productImages.addEventListener('click', handleClick);
 
 // ROUND COUNTER
-function roundCount(total,current){
-  if (current < total){
+function roundCount(total, current) {
+  if (current < total) {
     current++;
   } else {
     productImages.removeEventListener('click', handleClick);
@@ -119,7 +126,7 @@ function showResults() {
 }
 
 function findProducts(alt) {
-  for (let i =0; i< products.length; i++) {
+  for (let i = 0; i < products.length; i++) {
     if (products[i].name === alt) {
       products[i].clicks++;
     }
@@ -133,7 +140,7 @@ function renderNewProducts() {
   let index2 = Math.floor(Math.random() * products.length);
   let index3 = Math.floor(Math.random() * products.length);
   // chat gpt for this loop to avoid dupicates in the original. just the while loop*****
-  while(index1 === index2 || index1 === index3 || index2 === index3) {
+  while (index1 === index2 || index1 === index3 || index2 === index3) {
     index1 = Math.floor(Math.random() * products.length);
     index2 = Math.floor(Math.random() * products.length);
     index3 = Math.floor(Math.random() * products.length);
@@ -143,15 +150,15 @@ function renderNewProducts() {
   let randomProduct3 = products[index3];
 
   // render the products onto the page / add the name
-  renderImage(image1,randomProduct1);
-  renderImage(image2,randomProduct2);
-  renderImage(image3,randomProduct3);
-  // image1.setAttribute('src', randomProduct1.url);
-  // image2.setAttribute('src', randomProduct2.url);
-  // image3.setAttribute('src', randomProduct3.url);
-  // image1.setAttribute('alt', randomProduct1.name);
-  // image2.setAttribute('alt', randomProduct2.name);
-  // image3.setAttribute('alt', randomProduct3.name);
+  // renderImage(image1, randomProduct1);
+  // renderImage(image2, randomProduct2);
+  // renderImage(image3, randomProduct3);
+  image1.setAttribute('src', randomProduct1.url);
+  image2.setAttribute('src', randomProduct2.url);
+  image3.setAttribute('src', randomProduct3.url);
+  image1.setAttribute('alt', randomProduct1.name);
+  image2.setAttribute('alt', randomProduct2.name);
+  image3.setAttribute('alt', randomProduct3.name);
 }
 function getNames() {
   let names = [];
@@ -183,7 +190,7 @@ let button = document.getElementById('results-button');
 
 button.addEventListener('click', viewChart);
 
-const ctx = document.getElementById('myChart');
+// const ctx = document.getElementById('myChart');
 
 
 function viewChart() {
