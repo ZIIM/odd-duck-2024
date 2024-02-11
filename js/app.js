0dc088b0fa12d3e7cdc76f37ff24bfc95e6cbb6e
John Thomas Gause III <johntgause12@gmail.com>
2024-02-10 19:02:50 -0800

    json

--- js/app.js
+++ js/app.js
@@ -1,49 +1,24 @@
-'use strict';
+'use strict'
 
-// function saveData(data) {
-//   localStorage.setItem('goatData', JSON.stringify(data));
-// }
-
-// function fetchData() {
-// return JSON.parse(localStorage.getItem('goatData'));
-// }
-
-// function clearData() {
-//   localStorage.clear();
-// }
+console.log('JS is loaded');
 
 
-
-localStorage.setItem('productsData', JSON.stringify(products)); //method of the localStorage object that takes two arguments, the key and the value
+localStorage.setItem('productData', JSON.stringify(products)); //method of the localStorage object that takes two arguments, the key and the value
 
 console.log('The local storage object', localStorage);
 
-let stringFromStorage = localStorage.getItem('productsData'); //method of the localStorage object that takes one argument, the key
+let stringFromStorage = localStorage.getItem('productData'); //method of the localStorage object that takes one argument, the key
 console.log(stringFromStorage);
 
 let object = JSON.parse(stringFromStorage);
 console.log('PARSED BACK TO JS OBJECT', object);
-// follow up on this from line 3 to line 11, help with local storage *******
-localStorage.setItem('duckData', JSON.stringify(oddDucks)); //method of the localStorage object that takes two arguments, the key and the value
-
-console.log('The local storage object', localStorage);
-
-let stringFromStorage = localStorage.getItem('duckData'); //method of the localStorage object that takes one argument, the key
-console.log(stringFromStorage);
-
-let object = JSON.parse(stringFromStorage);
-console.log('PARSED BACK TO JS OBJECT', object);
-
-
-// ^^^^^^^^^^^^^^^^^
-
 
 function saveData(data) {
-  localStorage.setItem('productsData', JSON.stringify(data));
+  localStorage.setItem('productData', JSON.stringify(data));
 }
 
 function fetchData() {
-  JSON.parse(localStorage.getItem('productsData'));
+  JSON.stringify(localStorage.getItem('productData'));
 }
 
 function clearData() {
